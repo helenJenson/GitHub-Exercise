438c55e2e9c13345a9e14b4f6ca7c952b7cfaa52
helenJenson <hjenson@gmail.com>
2024-05-03 17:11:30 +0100

    Version 1 - red

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -10,12 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+   
+            Text("Hello, world")
+                .font(.title)
+            Text("V1.0")
+                .padding()
+               
+
         }
         .padding()
+        .foregroundStyle(.red)
     }
 }
 
